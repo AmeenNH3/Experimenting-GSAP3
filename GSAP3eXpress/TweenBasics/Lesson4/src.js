05055f10ef0d526346fc949dd716a1188a56c895
AmeenNH3 <md.ameenullah.h@gmail.com>
2023-01-23 18:18:57 +0530

    lesson 4 added

--- GSAP3eXpress/TweenBasics/Lesson4/src.js
+++ GSAP3eXpress/TweenBasics/Lesson4/src.js
@@ -1,7 +1,29 @@
-const button = document.querySelector(".btn");
-const bg = document.querySelector(".bg");
+const play = document.querySelector("#play");
+const pause = document.querySelector("#pause");
+const reverse = document.querySelector("#reverse");
+const restart = document.querySelector("#restart");
+const bg = document.querySelectorAll("#play .bg");
+const bg1 = document.querySelectorAll("#pause .bg");
+const bg2 = document.querySelectorAll("#reverse .bg");
+const bg3 = document.querySelectorAll("#restart .bg");
 
 let tween = gsap.fromTo(bg, { scale: 0 }, { scale: 1, duration: 1, ease: "circ" });
-button.addEventListener("mouseenter", function () {
+let tween1 = gsap.fromTo(bg1, { scale: 0 }, { scale: 1, duration: 1, ease: "circ" });
+let tween2 = gsap.fromTo(bg2, { scale: 0 }, { scale: 1, duration: 1, ease: "circ" });
+let tween3 = gsap.fromTo(bg3, { scale: 0 }, { scale: 1, duration: 1, ease: "circ" });
+
+play.addEventListener("mouseenter", function () {
   tween.restart();
+});
+
+pause.addEventListener("mouseenter", function () {
+  tween1.restart();
+});
+
+reverse.addEventListener("mouseenter", function () {
+  tween2.restart();
+});
+
+restart.addEventListener("mouseenter", function () {
+  tween3.restart();
 });
